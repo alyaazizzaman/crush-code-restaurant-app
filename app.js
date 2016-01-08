f2dab9beb20dd6eddd16dd723873c73efdac07d7
Alya Aziz-Zaman <alya.aziz.zaman@gmail.com>
2016-01-08 09:41:19 -0700

    created basic express app and set up server

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//require express
+var express = require('express'),
+app = express();
+
+//set up server
+app.listen(3000, function() {
+  console.log("Server is up and running!");
+});
