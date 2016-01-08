5cc4de23dc9b53f7afed4dcdac410a8ac54ef3ba
Alya Aziz-Zaman <alya.aziz.zaman@gmail.com>
2016-01-08 11:27:47 -0700

    required handlebarsand knex, set up postgresql database and connected it to app.js using knex

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 'use strict';
 
+//require dotenv
+require('dotenv').load();
+
 //require express
 var express = require('express'),
 app = express();
+
+//require handlebars
+var hbs = require('handlebars');
+app.set('view engine', 'hbs');
+
+//require knex
+var knex = require('knex')({
+  client: 'pg',
+  connection: process.env.DATABSE_URL,
+});
 
 //set up server
 app.listen(3000, function() {
